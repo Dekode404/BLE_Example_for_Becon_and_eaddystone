18db6e53d1c887ab9c6f3212e0c7ac2f4ca38104
saurabh kadam <ssk3@somaiya.edu>
2024-06-22 13:18:12 +0530

    Add all #include used for this project and from the menuconfig change the host mode to "Nimble BLE only"

--- main/main.c
+++ main/main.c
@@ -1,4 +1,4 @@
-/* Hello World Example
+/* BLE beacon & eddy-stone example
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -6,9 +6,14 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <stdio.h>                       /* This is the standard C lib used for the printf statement in this project */
+#include <nvs_flash.h>                   /* This is ESP lib used to initiate the NVS flsh used for the bluetooth application */
+#include <esp_log.h>                     /* This is ESP lib used to loig the error */
+#include <esp_nimble_hci.h>              /* This is ESP lib used for the HOST and CONTROLLER interface */
+#include <nimble/nimble_port.h>          /* This is ESP lib used for initiate the nimbale port for the bluetooth application */
+#include <nimble/nimble_port_freertos.h> /* This is ESP lib used for create the task for the nimble bluetooth application */
+#include <host/ble_hs.h>                 /* This is ESP lib used for the ble host controller */
+#include <services/gap/ble_svc_gap.h>    /* This is ESP lib used for initiate the ble service */
 
 void app_main(void)
 {
